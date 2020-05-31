f70594499564a8709d4552c671d1230d92fc9198
Raphael Ferreira <rpfj>
2020-05-31 12:31:44 -0300

    [DDL_DB_TCC] adicionado novo script do banco compilado

--- Banco de transacional - Cooperativas/SCRIPT_COMPILADO.sql
+++ Banco de transacional - Cooperativas/SCRIPT_COMPILADO.sql
@@ -21,7 +21,7 @@ USE `empreendimentos` ;
 -- Table `empreendimentos`.`cdt_apoiador`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_apoiador` (
-  `idApoiador` INT NOT NULL AUTO_INCREMENT,
+  `idApoiador` INT(11) NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NULL DEFAULT NULL,
   `cidade` VARCHAR(100) NULL DEFAULT NULL,
   `tipo_de_ajuda` VARCHAR(100) NULL DEFAULT NULL,
@@ -36,7 +36,7 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_area_atuacao`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_area_atuacao` (
-  `idAreaAtuacao` INT NOT NULL AUTO_INCREMENT,
+  `idAreaAtuacao` INT(11) NOT NULL AUTO_INCREMENT,
   `desc_area_atuacao` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`idAreaAtuacao`))
 ENGINE = InnoDB
@@ -48,12 +48,11 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_atividade_emprrendimento`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_atividade_emprrendimento` (
-  `idAtividade` INT NOT NULL AUTO_INCREMENT,
+  `idAtividade` INT(11) NOT NULL AUTO_INCREMENT,
   `desc_atividade` VARCHAR(200) NULL DEFAULT NULL,
   PRIMARY KEY (`idAtividade`),
   INDEX `fk_Atividade_Empreendimento` (`desc_atividade` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = latin1;
 
 
@@ -61,7 +60,7 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_localizacao`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_localizacao` (
-  `idLocalizacao` INT NOT NULL AUTO_INCREMENT,
+  `idLocalizacao` INT(11) NOT NULL AUTO_INCREMENT,
   `desc_localizacao` VARCHAR(500) NULL DEFAULT NULL,
   PRIMARY KEY (`idLocalizacao`))
 ENGINE = InnoDB
@@ -73,7 +72,7 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_tp_organizacao`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_tp_organizacao` (
-  `idTipoOrganizacao` INT NOT NULL AUTO_INCREMENT,
+  `idTipoOrganizacao` INT(11) NOT NULL AUTO_INCREMENT,
   `nome_tipo` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`idTipoOrganizacao`))
 ENGINE = InnoDB
@@ -85,18 +84,18 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_empreendimento`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_empreendimento` (
-  `idEmpreendimento` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NULL DEFAULT NULL,
+  `idEmpreendimento` INT(11) NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(100) NULL DEFAULT NULL,
   `endereco` VARCHAR(500) NULL DEFAULT NULL,
   `cidade` VARCHAR(100) NULL DEFAULT NULL,
   `latitude` FLOAT NULL DEFAULT NULL,
   `longitude` FLOAT NULL DEFAULT NULL,
   `senha` VARCHAR(8) NULL DEFAULT NULL,
   `sigla` VARCHAR(20) NULL DEFAULT NULL,
-  `ano_inicio` INT NULL DEFAULT NULL,
-  `id_area_atuacao` INT NULL DEFAULT NULL,
-  `id_localizacao` INT NULL DEFAULT NULL,
-  `id_tipo_organizacao` INT NULL DEFAULT NULL,
+  `ano_inicio` INT(11) NULL DEFAULT NULL,
+  `id_area_atuacao` INT(11) NULL DEFAULT NULL,
+  `id_localizacao` INT(11) NULL DEFAULT NULL,
+  `id_tipo_organizacao` INT(11) NULL DEFAULT NULL,
   `dt_cadastro` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `email` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`idEmpreendimento`),
@@ -122,7 +121,7 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_tp_contato`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_tp_contato` (
-  `idtipo_contato` INT NOT NULL AUTO_INCREMENT,
+  `idtipo_contato` INT(11) NOT NULL AUTO_INCREMENT,
   `nome_tipo` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`idtipo_contato`))
 ENGINE = InnoDB
@@ -134,10 +133,10 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_contato`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_contato` (
-  `idContato` INT NOT NULL AUTO_INCREMENT,
-  `id_tipo_contato` INT NULL DEFAULT NULL,
+  `idContato` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_tipo_contato` INT(11) NULL DEFAULT NULL,
   `desc_contato` VARCHAR(100) NULL DEFAULT NULL,
-  `id_empreendimento` INT NULL DEFAULT NULL,
+  `id_empreendimento` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idContato`),
   INDEX `fk_contato_empreendimento` (`id_empreendimento` ASC) VISIBLE,
   INDEX `fk_contato_tp_contato` (`id_tipo_contato` ASC) VISIBLE,
@@ -156,7 +155,7 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_formacao_coletiva`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_formacao_coletiva` (
-  `idFormacao_coletiva` INT NOT NULL AUTO_INCREMENT,
+  `idFormacao_coletiva` INT(11) NOT NULL AUTO_INCREMENT,
   `desc_formacao_coletiva` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`idFormacao_coletiva`))
 ENGINE = InnoDB
@@ -165,10 +164,23 @@ DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
+-- Table `empreendimentos`.`cdt_geracao_renda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_geracao_renda` (
+  `idGeracao_renda` INT(11) NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`idGeracao_renda`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
 -- Table `empreendimentos`.`cdt_questao`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_questao` (
-  `idQuestao` INT NOT NULL AUTO_INCREMENT,
+  `idQuestao` INT(11) NOT NULL AUTO_INCREMENT,
   `desc_questao` TEXT NULL DEFAULT NULL,
   PRIMARY KEY (`idQuestao`))
 ENGINE = InnoDB
@@ -180,9 +192,9 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_opcao_questao`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_opcao_questao` (
-  `idOpcao` INT NOT NULL AUTO_INCREMENT,
+  `idOpcao` INT(11) NOT NULL AUTO_INCREMENT,
   `desc_opcao` TEXT NULL DEFAULT NULL,
-  `id_questao` INT NULL DEFAULT NULL,
+  `id_questao` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idOpcao`),
   INDEX `fk_opcao_questao_questao` (`id_questao` ASC) VISIBLE,
   CONSTRAINT `fk_opcao_questao_questao`
@@ -197,7 +209,7 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_tp_rede`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_tp_rede` (
-  `idtipo_rede` INT NOT NULL AUTO_INCREMENT,
+  `idtipo_rede` INT(11) NOT NULL AUTO_INCREMENT,
   `desc_tipo` VARCHAR(500) NULL DEFAULT NULL,
   PRIMARY KEY (`idtipo_rede`),
   UNIQUE INDEX `desc_tipo` (`desc_tipo` ASC) VISIBLE)
@@ -210,11 +222,11 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_rede_economica`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_rede_economica` (
-  `idRede_economica` INT NOT NULL AUTO_INCREMENT,
-  `desc_rede` VARCHAR(100) NULL DEFAULT NULL,
+  `idRede_economica` INT(11) NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(100) NULL DEFAULT NULL,
   `latitude` FLOAT NULL DEFAULT NULL,
   `longitude` FLOAT NULL DEFAULT NULL,
-  `id_tipo_rede` INT NULL DEFAULT NULL,
+  `id_tipo_rede` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idRede_economica`),
   INDEX `fk_rede_economica_1` (`id_tipo_rede` ASC) VISIBLE,
   CONSTRAINT `fk_rede_economica_1`
@@ -229,15 +241,15 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`rel_evolucao`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`rel_evolucao` (
-  `idEvolucao` INT NOT NULL AUTO_INCREMENT,
+  `idEvolucao` INT(11) NOT NULL AUTO_INCREMENT,
   `data` DATE NULL DEFAULT NULL,
-  `geracao_renda` FLOAT NULL DEFAULT NULL,
-  `n_homens` INT NULL DEFAULT NULL,
-  `n_mulheres` INT NULL DEFAULT NULL,
+  `id_geracao_renda` INT(11) NOT NULL,
+  `n_homens` INT(11) NULL DEFAULT NULL,
+  `n_mulheres` INT(11) NULL DEFAULT NULL,
   `computador` TINYINT(1) NULL DEFAULT NULL,
   `internet` TINYINT(1) NULL DEFAULT NULL,
-  `id_empreendimento` INT NULL DEFAULT NULL,
-  `id_rede_economica` INT NULL DEFAULT NULL,
+  `id_empreendimento` INT(11) NULL DEFAULT NULL,
+  `id_rede_economica` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idEvolucao`),
   INDEX `fk_evolucao_empreendimento` (`id_empreendimento` ASC) VISIBLE,
   INDEX `fk_evolucao_rede_economica` (`id_rede_economica` ASC) VISIBLE,
@@ -248,6 +260,7 @@ CREATE TABLE IF NOT EXISTS `empreendimentos`.`rel_evolucao` (
     FOREIGN KEY (`id_rede_economica`)
     REFERENCES `empreendimentos`.`cdt_rede_economica` (`idRede_economica`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = latin1;
 
 
@@ -255,19 +268,25 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_resposta`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_resposta` (
-  `idResposta` INT NOT NULL AUTO_INCREMENT,
-  `desc_resposta` VARCHAR(500) NULL DEFAULT NULL,
-  `id_questao` INT NULL DEFAULT NULL,
-  `id_evolucao` INT NULL DEFAULT NULL,
+  `idResposta` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_opcao_questao` INT(11) NOT NULL,
+  `id_questao` INT(11) NOT NULL,
+  `id_evolucao` INT(11) NOT NULL,
   PRIMARY KEY (`idResposta`),
   INDEX `fk_respostas_evolucao` (`id_evolucao` ASC) VISIBLE,
   INDEX `fk_resposta_questao` (`id_questao` ASC) VISIBLE,
+  INDEX `fk_respostas_opcao_questao_idx` (`id_opcao_questao` ASC) VISIBLE,
   CONSTRAINT `fk_resposta_questao`
     FOREIGN KEY (`id_questao`)
     REFERENCES `empreendimentos`.`cdt_questao` (`idQuestao`),
   CONSTRAINT `fk_respostas_evolucao`
     FOREIGN KEY (`id_evolucao`)
-    REFERENCES `empreendimentos`.`rel_evolucao` (`idEvolucao`))
+    REFERENCES `empreendimentos`.`rel_evolucao` (`idEvolucao`),
+  CONSTRAINT `fk_respostas_opcao_questao`
+    FOREIGN KEY (`id_opcao_questao`)
+    REFERENCES `empreendimentos`.`cdt_opcao_questao` (`idOpcao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
@@ -276,7 +295,7 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`cdt_usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_usuarios` (
-  `idUsuario` INT NOT NULL AUTO_INCREMENT,
+  `idUsuario` INT(11) NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NULL DEFAULT NULL,
   `sobrenome` VARCHAR(100) NULL DEFAULT NULL,
   `email` VARCHAR(100) NULL DEFAULT NULL,
@@ -284,7 +303,6 @@ CREATE TABLE IF NOT EXISTS `empreendimentos`.`cdt_usuarios` (
   `dt_cadastro` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`idUsuario`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = latin1;
 
 
@@ -292,16 +310,16 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`formacao_coletiva_emprrendimento`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`formacao_coletiva_emprrendimento` (
-  `id_formacao_coletiva` INT NOT NULL,
-  `id_empreendimento` INT NOT NULL,
+  `id_formacao_coletiva` INT(11) NOT NULL,
+  `id_empreendimento` INT(11) NOT NULL,
   PRIMARY KEY (`id_formacao_coletiva`, `id_empreendimento`),
   INDEX `fk_Formacao_coletiva_has_empreendimento_empreendimento1` (`id_empreendimento` ASC) VISIBLE,
-  CONSTRAINT `fk_Formacao_coletiva_has_empreendimento_empreendimento1`
-    FOREIGN KEY (`id_empreendimento`)
-    REFERENCES `empreendimentos`.`cdt_empreendimento` (`idEmpreendimento`),
   CONSTRAINT `fk_Formacao_coletiva_has_empreendimento_Formacao_coletiva1`
     FOREIGN KEY (`id_formacao_coletiva`)
-    REFERENCES `empreendimentos`.`cdt_formacao_coletiva` (`idFormacao_coletiva`))
+    REFERENCES `empreendimentos`.`cdt_formacao_coletiva` (`idFormacao_coletiva`),
+  CONSTRAINT `fk_Formacao_coletiva_has_empreendimento_empreendimento1`
+    FOREIGN KEY (`id_empreendimento`)
+    REFERENCES `empreendimentos`.`cdt_empreendimento` (`idEmpreendimento`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
@@ -310,20 +328,16 @@ DEFAULT CHARACTER SET = latin1;
 -- Table `empreendimentos`.`rede_economica_apoiador`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `empreendimentos`.`rede_economica_apoiador` (
-  `id_rede_economica` INT NOT NULL,
-  `id_apoiador` INT NOT NULL,
+  `id_rede_economica` INT(11) NOT NULL,
+  `id_apoiador` INT(11) NOT NULL,
   PRIMARY KEY (`id_rede_economica`, `id_apoiador`),
   INDEX `fk_rede_economica_apoiador_apoiador_idx` (`id_apoiador` ASC) VISIBLE,
-  CONSTRAINT `fk_rede_economica_apoiador_r_economica`
-    FOREIGN KEY (`id_rede_economica`)
-    REFERENCES `empreendimentos`.`cdt_rede_economica` (`idRede_economica`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_rede_economica_apoiador_apoiador`
     FOREIGN KEY (`id_apoiador`)
-    REFERENCES `empreendimentos`.`cdt_apoiador` (`idApoiador`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `empreendimentos`.`cdt_apoiador` (`idApoiador`),
+  CONSTRAINT `fk_rede_economica_apoiador_r_economica`
+    FOREIGN KEY (`id_rede_economica`)
+    REFERENCES `empreendimentos`.`cdt_rede_economica` (`idRede_economica`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
